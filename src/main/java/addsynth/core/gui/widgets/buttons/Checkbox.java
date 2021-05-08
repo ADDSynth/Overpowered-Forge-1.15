2d47b7696b401f6ae8ac0fd6b6337454a99266c8
ADDSynth <addsynth@gmail.com>
2021-05-08 16:33:53 -0500

    Gui Update 2

--- src/main/java/addsynth/core/gui/widgets/buttons/Checkbox.java
+++ src/main/java/addsynth/core/gui/widgets/buttons/Checkbox.java
@@ -1,22 +1,22 @@
 package addsynth.core.gui.widgets.buttons;
 
 import addsynth.core.ADDSynthCore;
-import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.Minecraft;
+import addsynth.core.gui.util.GuiUtil;
+import addsynth.core.gui.widgets.WidgetUtil;
 import net.minecraft.client.gui.widget.button.AbstractButton;
 import net.minecraft.util.ResourceLocation;
 
-public abstract class CheckBox extends AbstractButton {
+public abstract class Checkbox extends AbstractButton {
 
   private static final ResourceLocation texture = new ResourceLocation(ADDSynthCore.MOD_ID,"textures/gui/gui_textures.png");
   private static final int texture_x = 0;
   private static final int texture_y = 32;
   private static final int texture_width = 24;
   private static final int texture_height = 24;
+  public static final int gui_size = 12;
 
-  public CheckBox(final int x, final int y){
-    super(x, y, 12, 12, "");
+  public Checkbox(final int x, final int y, String label){
+    super(x, y, gui_size, gui_size, label);
   }
 
   protected abstract boolean get_toggle_state();
@@ -24,14 +24,9 @@ public abstract class CheckBox extends AbstractButton {
   @Override
   public final void renderButton(final int mouseX, final int mouseY, final float partial_ticks){
     final boolean checked = get_toggle_state();
-    Minecraft.getInstance().getTextureManager().bindTexture(texture);
-    RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-
-    RenderSystem.enableBlend();
-    RenderSystem.defaultBlendFunc();
-    RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
-
-    blit(x, y, 12, 12, checked ? texture_x : texture_x + 24, texture_y, texture_width, texture_height, 256, 256);
+    WidgetUtil.common_button_render_setup(texture);
+    blit(x, y, gui_size, gui_size, checked ? texture_x : texture_x + texture_height, texture_y, texture_width, texture_height, 256, 256);
+    GuiUtil.draw_text_left(getMessage(), x + 16, y + 2);
   }
 
 }
