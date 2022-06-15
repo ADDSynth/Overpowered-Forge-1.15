f91d6f6cd872b13d1bc22437890e95124e547c20
ADDSynth <addsynth@gmail.com>
2022-06-14 22:59:17 -0500

    Fixed misaligned text in Rotate button

--- src/main/java/addsynth/core/gui/widgets/buttons/AdjustableButton.java
+++ src/main/java/addsynth/core/gui/widgets/buttons/AdjustableButton.java
@@ -1,7 +1,9 @@
 package addsynth.core.gui.widgets.buttons;
 
 import javax.annotation.Nonnull;
+import addsynth.core.ADDSynthCore;
 import addsynth.core.gui.widgets.WidgetUtil;
+import addsynth.core.util.StringUtil;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
@@ -23,7 +25,10 @@ public abstract class AdjustableButton extends AbstractButton {
    * @param height
    */
   public AdjustableButton(int x, int y, int width, int height){
-    super(x, y, width, height, "");
+    super(x, y, width, Math.min(height, max_height), "");
+    if(height > max_height){
+      ADDSynthCore.log.warn(StringUtil.build("Cannot set height of ", AdjustableButton.class.getSimpleName(), " higher than ", Integer.toString(max_height), "."));
+    }
   }
 
   /**
@@ -35,7 +40,10 @@ public abstract class AdjustableButton extends AbstractButton {
    * @param buttonText
    */
   public AdjustableButton(int x, int y, int width, int height, @Nonnull String buttonText){
-    super(x, y, width, height, buttonText);
+    super(x, y, width, Math.min(height, max_height), buttonText);
+    if(height > max_height){
+      ADDSynthCore.log.warn(StringUtil.build("Cannot set height of ", AdjustableButton.class.getSimpleName(), " higher than ", Integer.toString(max_height), "."));
+    }
   }
 
     /**
