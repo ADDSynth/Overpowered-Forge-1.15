7b2cd170642b4aacc605092380e32107aaad21cd
ADDSynth <addsynth@gmail.com>
2022-08-11 15:18:57 -0500

    Optimized Lasers

--- src/main/java/addsynth/core/util/constants/DirectionConstant.java
+++ src/main/java/addsynth/core/util/constants/DirectionConstant.java
@@ -3,6 +3,8 @@ package addsynth.core.util.constants;
 import javax.annotation.Nonnegative;
 import addsynth.core.ADDSynthCore;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 
 public final class DirectionConstant {
 
@@ -25,6 +27,16 @@ public final class DirectionConstant {
       return (Math.abs(direction) + 1) % 6;
     }
     return (Math.abs(direction) - 1) % 6;
+  }
+
+  public static final Direction getDirection(final BlockPos current_position, final BlockPos adjacent_position){
+    if(current_position.equals(adjacent_position)){
+      ADDSynthCore.log.error(DirectionConstant.class.getName()+".getDirection(BlockPos, BlockPos) cannot determine direction because both BlockPositions are equal!");
+    }
+    final int x = MathHelper.clamp(adjacent_position.getX() - current_position.getX(), -1, 1);
+    final int y = MathHelper.clamp(adjacent_position.getY() - current_position.getY(), -1, 1);
+    final int z = MathHelper.clamp(adjacent_position.getZ() - current_position.getZ(), -1, 1);
+    return Direction.byLong(x, y, z);
   }
 
 }
--- src/main/java/addsynth/overpoweredmod/machines/laser/cannon/LaserCannon.java
+++ src/main/java/addsynth/overpoweredmod/machines/laser/cannon/LaserCannon.java
@@ -25,7 +25,6 @@ import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.StateContainer.Builder;
 import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
@@ -177,17 +176,6 @@ public final class LaserCannon extends Block implements IWaterLoggable {
   @SuppressWarnings("deprecation")
   public IFluidState getFluidState(BlockState state){
     return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
-  }
-
-  @Override
-  public boolean hasTileEntity(BlockState state){
-    return color >= 0;
-  }
-
-  @Override
-  @Nullable
-  public final TileEntity createTileEntity(BlockState state, IBlockReader world){
-    return color == -1 ? null : new TileLaser();
   }
 
   @Override
